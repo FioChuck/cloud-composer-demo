8036aba87372f2ec319fb755f3284651f85c6650
FiocChuck <chasf@google.com>
2023-03-20 15:47:39 -0400

    deploy

--- dags/spark-bq.py
+++ dags/spark-bq.py
@@ -1,11 +1,20 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.providers.google.cloud.operators.dataproc import (
-    DataprocCreateBatchOperator, DataprocGetBatchOperator)
+    DataprocCreateBatchOperator, DataprocGetBatchOperator, DataprocSubmitJobOperator)
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 
 args = {
     'owner': 'packt-developer',
+}
+
+SPARK_JOB = {
+    "reference": {"project_id": "cf-data-analytics"},
+    "placement": {"cluster_name": "cluster-a7bd"},
+    "spark_job": {
+        "jar_file_uris": ["gs://cf-spark-jobs/template/scala-2.12/file-creator-assembly-1.0.jar"],
+        "main_class": "source.main.scala.BqDemo",
+    },
 }
 
 with DAG(
@@ -15,14 +24,9 @@ with DAG(
     start_date=days_ago(1),
 ) as dag:
 
-    spark_task = {
-        "reference": {"project_id": "cf-data-analytics"},
-        "placement": {"cluster_name": "cluster-a7bd"},
-        "spark_job": {
-            "jar_file_uris": ["gs://cf-spark-jobs/template/scala-2.12/file-creator-assembly-1.0.jar"],
-            "main_class": "source.main.scala.BqDemo",
-        },
-    }
+    spark_task = DataprocSubmitJobOperator(
+        task_id="spark_task", job=SPARK_JOB, region="us-central1", project_id="cf-data-analytics"
+    )
 
     spark_task
 
