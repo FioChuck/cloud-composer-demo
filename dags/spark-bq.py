717d80ad37aba0308570498be2b379b1fb85a307
FiocChuck <chasf@google.com>
2023-03-21 12:03:50 -0400

    added lineage to airflow

--- dags/spark-bq.py
+++ dags/spark-bq.py
@@ -3,6 +3,8 @@ from airflow.utils.dates import days_ago
 from airflow.providers.google.cloud.operators.dataproc import (
     DataprocCreateBatchOperator, DataprocGetBatchOperator, DataprocSubmitJobOperator)
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.composer.data_lineage.entities import BigQueryTable
+
 
 args = {
     'owner': 'packt-developer',
@@ -25,7 +27,17 @@ with DAG(
 ) as dag:
 
     spark_task = DataprocSubmitJobOperator(
-        task_id="spark_task", job=SPARK_JOB, region="us-central1", project_id="cf-data-analytics"
+        task_id="spark_task", job=SPARK_JOB, region="us-central1", project_id="cf-data-analytics",
+        inlets=[BigQueryTable(
+            project_id="cf-data-analytics",
+            dataset_id='market_data',
+            table_id='googl_spark_ingestion',
+        )],
+        outlets=[BigQueryTable(
+            project_id="cf-data-analytics",
+            dataset_id='market_data',
+            table_id='googl_spark_ingestion',
+        )]
     )
 
     spark_task
