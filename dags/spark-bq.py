2ce8dc5e017aa9484e21a1710787ceb5a12dee6d
FiocChuck <chasf@google.com>
2024-10-03 16:53:22 -0400

    switch back to cluster

--- dags/spark-bq.py
+++ dags/spark-bq.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
-# from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitJobOperator
+from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitJobOperator
 from airflow.providers.google.cloud.operators.dataproc import DataprocCreateBatchOperator, DataprocListBatchesOperator
 import uuid
 
@@ -10,14 +10,14 @@ args = {
 }
 
 
-# SPARK_JOB = {
-#     "reference": {"project_id": "cf-data-analytics"},
-#     "placement": {"cluster_name": "cluster-f866"},
-#     "spark_job": {
-#         "jar_file_uris": ["gs://cf-spark-jobs/spark-stock-transformations/scala-2.13/spark-window-functions-assembly-3.0.jar"],
-#         "main_class": "Main",
-#     },
-# }
+SPARK_JOB = {
+    "reference": {"project_id": "cf-data-analytics"},
+    "placement": {"cluster_name": "cluster-f866"},
+    "spark_job": {
+        "jar_file_uris": ["gs://cf-spark-jobs/spark-stock-transformations/scala-2.13/spark-window-functions-assembly-3.0.jar"],
+        "main_class": "Main",
+    },
+}
 
 with DAG(
     dag_id='spark-bq',
@@ -28,39 +28,39 @@ with DAG(
     is_paused_upon_creation=False
 ) as dag:
 
-    name = str(uuid.uuid4())
+    # name = str(uuid.uuid4())
 
-    list_batches = DataprocListBatchesOperator(
-        task_id="list-all-batches",
-        project_id="cf-data-analytics",
-        region="us-central1"
-    )
+    # list_batches = DataprocListBatchesOperator(
+    #     task_id="list-all-batches",
+    #     project_id="cf-data-analytics",
+    #     region="us-central1"
+    # )
 
-    create_batch = DataprocCreateBatchOperator(
-        task_id="batch_create",
-        project_id="cf-data-analytics",
-        region="us-central1",
-        batch={
-            "spark_batch": {
-                "main_jar_file_uri": "gs://cf-spark-jobs/spark-stock-transformations/scala-2.12/spark-window-functions-assembly-3.0.jar"
-            },
-            "runtime_config": {
-                "version": "1.1",
-                "properties": {"spark.dataproc.lineage.enabled": "true"}
-            }
-        },
-        batch_id=name,
-    )
+    # create_batch = DataprocCreateBatchOperator(
+    #     task_id="batch_create",
+    #     project_id="cf-data-analytics",
+    #     region="us-central1",
+    #     batch={
+    #         "spark_batch": {
+    #             "main_jar_file_uri": "gs://cf-spark-jobs/spark-stock-transformations/scala-2.12/spark-window-functions-assembly-3.0.jar"
+    #         },
+    #         "runtime_config": {
+    #             "version": "1.1",
+    #             "properties": {"spark.dataproc.lineage.enabled": "true"}
+    #         }
+    #     },
+    #     batch_id=name,
+    # )
 
     #
 
-    # spark_task = DataprocSubmitJobOperator(
-    #     task_id="spark_task", job=SPARK_JOB, region="us-central1", project_id="cf-data-analytics")
+    spark_task = DataprocSubmitJobOperator(
+        task_id="spark_task", job=SPARK_JOB, region="us-central1", project_id="cf-data-analytics")
 
-    # spark_task
+    spark_task
 
-    list_batches >> create_batch
+    # list_batches >> create_batch
 
 if __name__ == "__main__":
-    # dag.cli()
-    dag.test()
+    dag.cli()
+    # dag.test()
